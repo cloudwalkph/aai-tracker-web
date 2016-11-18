fe2dfc772f1392a3a201fca5b2a40517f4b40246
Alleo Indong <freedom_war550@live.com.ph>
2016-11-18 12:40:49 +0800

    added hashid

---
+++ config/hashids.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of Laravel Hashids.
+ *
+ * (c) Vincent Klaiber <hello@vinkla.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Connection Name
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which of the connections below you wish to use as
+    | your default connection for all work. Of course, you may use many
+    | connections at once using the manager class.
+    |
+    */
+
+    'default' => 'main',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Hashids Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here are each of the connections setup for your application. Example
+    | configuration has been included, but you may add as many connections as
+    | you would like.
+    |
+    */
+
+    'connections' => [
+
+        'main' => [
+            'salt' => '03d67c263c27a453ef65b29e30334727333ccbcd',
+            'length' => 10,
+            'alphabet' => 'ads88a7sdad79adjo132poio64098ci23iu5oo23ioion1231o2u3n3098fs8sd6fshs0dv7sd',
+        ],
+
+        'alternative' => [
+            'salt' => 'your-salt-string',
+            'length' => 'your-length-integer',
+            'alphabet' => 'your-alphabet-string',
+        ],
+
+    ],
+
+];
