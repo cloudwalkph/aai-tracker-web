b31c324d504363cb0a545d9004b4162af446c56d
Alleo Indong <freedom_war550@live.com.ph>
2016-11-18 16:40:12 +0800

    created events migration and event

---
+++ app/Event.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Event extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'events';
+    protected $fillable = ['name', 'description', 'start_date', 'end_date'];
+
+    public function uniqueId()
+    {
+        return \Hashids::encode($this->id);
+    }
+
+    public function originalId($value)
+    {
+        return \Hashids::decode($value)[0];
+    }
+}
