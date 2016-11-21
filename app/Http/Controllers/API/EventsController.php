1f781c336acb5e3df06a73a8c57b0b36989f109b
Alleo Indong <freedom_war550@live.com.ph>
2016-11-21 16:52:13 +0800

    events service

---
+++ app/Http/Controllers/API/EventsController.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+
+class EventsController extends Controller {
+    public function getEvents()
+    {
+
+    }
+}
