e4698b0dc3ef6fbf79cdbb8e4215bb45f185457c
Alleo Indong <freedom_war550@live.com.ph>
2016-11-23 12:34:05 +0800

    updated controller

--- app/AAI/Services/EventsService.php
+++ app/AAI/Services/EventsService.php
@@ -48,27 +48,6 @@ class EventsService {
         return $result;
     }
 
-    public function saveAnswer($input)
-    {
-        if (! isset($input['event_id'])) {
-            throw new \Exception("Missing Event Id");
-        }
-
-        if (! isset($input['poll_id'])) {
-            throw new \Exception("Missing Poll Id");
-        }
-
-        if (! isset($input['event_location_id'])) {
-            throw new \Exception("Missing Event Location Id");
-        }
-
-        if (! isset($input['value'])) {
-            throw new \Exception("Missing Answer Value");
-        }
-
-        return EventAnswer::create($input);
-    }
-
     private function getPolls($eventId)
     {
         $eventPolls = $this->eventPolls->findByKey('event_id', $eventId)->get();
--- app/Http/Controllers/API/EventAnswersController.php
+++ app/Http/Controllers/API/EventAnswersController.php
@@ -1,23 +1,32 @@
 <?php
 namespace App\Http\Controllers\API;
 
+use App\AAI\Services\EventAnswersService;
 use App\AAI\Services\EventsService;
+use App\AAI\Services\ImageToS3Service;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SaveAnswerRequest;
 
 class EventAnswersController extends Controller {
     protected $eventService;
+    protected $eventAnswerService;
+    protected $imageService;
 
-    public function __construct(EventsService $eventService)
+    public function __construct(EventsService $eventService, EventAnswersService $eventAnswersService,
+                ImageToS3Service $imageService)
     {
         $this->eventService = $eventService;
+        $this->eventAnswerService = $eventAnswersService;
+        $this->imageService = $imageService;
     }
 
     public function saveAnswer(SaveAnswerRequest $request, $eventId)
     {
         $input = $request->all();
         $input['event_id'] = $eventId;
-        $answer = $this->eventService->saveAnswer($input);
+        $input['image'] = $this->imageService->processImage($request->file('image'));
+
+        $answer = $this->eventAnswerService->saveAnswer($input);
 
         return response()->json(['status' => 200, 'data' => $answer], 200);
     }
