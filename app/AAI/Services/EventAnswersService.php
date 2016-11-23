a7d64cce40c9c2e0561811fcc8e1d8b109055bb8
Alleo Indong <freedom_war550@live.com.ph>
2016-11-23 12:21:42 +0800

    save answer algorithm

--- app/AAI/Services/EventAnswersService.php
+++ app/AAI/Services/EventAnswersService.php
@@ -2,15 +2,20 @@
 namespace App\AAI\Services;
 
 use App\AAI\Modules\EventAnswers\Repositories\EventAnswersRepository;
+use App\AAI\Modules\EventLocationAnswers\Repositories\EventLocationAnswersRepository;
 use App\Models\EventAnswer;
+use App\Models\EventLocationAnswer;
 
 class EventAnswersService {
 
     protected $eventAnswer;
+    protected $eventLocationAnswer;
 
-    public function __construct(EventAnswersRepository $eventAnswer)
+    public function __construct(EventAnswersRepository $eventAnswer,
+                                EventLocationAnswersRepository $eventLocationAnswer)
     {
         $this->eventAnswer = $eventAnswer;
+        $this->eventLocationAnswer = $eventLocationAnswer;
     }
 
 
@@ -20,18 +25,38 @@ class EventAnswersService {
             throw new \Exception("Missing Event Id");
         }
 
-        if (! isset($input['poll_id'])) {
-            throw new \Exception("Missing Poll Id");
-        }
-
         if (! isset($input['event_location_id'])) {
             throw new \Exception("Missing Event Location Id");
         }
 
-        if (! isset($input['value'])) {
-            throw new \Exception("Missing Answer Value");
+        if (! isset($input['answers'])) {
+            throw new \Exception("Answers Missing");
         }
 
-        return EventAnswer::create($input);
+        $result = null;
+        \DB::transaction(function() use ($input) {
+            $eventLocationAnswerData = [
+                'uuid'          => $input['uuid'],
+                'event_id'      => $input['event_id'],
+                'user_id'       => $input['user_id'],
+                'event_location_id' => $input['event_location_id'],
+                'image'         => $input['image']
+            ];
+
+            $result = EventLocationAnswer::create($eventLocationAnswerData);
+
+            // Create answers
+            foreach ($result as $answer) {
+                $eventAnswer = EventAnswer::create([
+                    'poll_id'                   => $answer['poll_id'],
+                    'event_location_answer_id'  => $result['id'],
+                    'value'                     => $answer['value']
+                ]);
+
+                $result['answers'][] = $eventAnswer;
+            }
+        });
+
+        return $result;
     }
 }
