6ec2122d23a8f1fd477e1445dae6f3ad9fe7637f
Alleo Indong <freedom_war550@live.com.ph>
2016-11-23 16:50:29 +0800

    updated service

--- app/Http/Controllers/API/EventAnswersController.php
+++ app/Http/Controllers/API/EventAnswersController.php
@@ -6,6 +6,7 @@ use App\AAI\Services\EventsService;
 use App\AAI\Services\ImageToS3Service;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SaveAnswerRequest;
+use App\Http\Requests\UploadImageRequest;
 
 class EventAnswersController extends Controller {
     protected $eventService;
@@ -20,18 +21,24 @@ class EventAnswersController extends Controller {
         $this->imageService = $imageService;
     }
 
+    public function uploadImage(UploadImageRequest $request)
+    {
+        $file = $request->file('image');
+
+        $fileName = $this->imageService->processImage($file);
+
+        return response()->json(['status' => 201, 'data' => ['file_name' => $fileName]]);
+    }
+
     public function saveAnswer(SaveAnswerRequest $request, $eventId, $locationId)
     {
         $input = $request->all();
         $input['event_id'] = $eventId;
         $input['event_location_id'] = $locationId;
 
-        // Upload the image and get the name
-        $input['image'] = $this->imageService->processImage($request->file('image'));
-
         // Save the answer
         $answer = $this->eventAnswerService->saveAnswer($input);
 
-        return response()->json(['status' => 200, 'data' => $answer], 200);
+        return response()->json(['status' => 201, 'data' => $answer], 200);
     }
 }
--- app/Http/Requests/UploadImageRequest.php
+++ app/Http/Requests/UploadImageRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class SaveAnswerRequest extends FormRequest
+class UploadImageRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,12 +24,7 @@ class SaveAnswerRequest extends FormRequest
     public function rules()
     {
         return [
-            'user_id'           => 'required',
-            'uuid'              => 'required',
-            'image'             => 'required',
-            'answers'           => 'required',
-            'answers.*.poll_id' => 'required|exists:polls,id',
-            'answers.*.value'   => 'required'
+            'image'             => 'required|image',
         ];
     }
 
