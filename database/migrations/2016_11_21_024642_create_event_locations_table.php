8972769844ee359f4091847d4045f504d0f0eb22
Alleo Indong <freedom_war550@live.com.ph>
2016-11-29 15:56:13 +0800

    no default

--- database/migrations/2016_11_21_024642_create_event_locations_table.php
+++ database/migrations/2016_11_21_024642_create_event_locations_table.php
@@ -18,7 +18,7 @@ class CreateEventLocationsTable extends Migration
             $table->integer('event_id')->unsigned();
             $table->string('name');
             $table->integer('expected_hits')->unsigned();
-            $table->string('ip');
+            $table->string('ip')->default('');
             $table->timestamps();
             $table->softDeletes();
 
