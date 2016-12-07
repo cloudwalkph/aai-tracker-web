b45c3f427a6bc6309747b139b92f458bbca877c0
Alleo Indong <freedom_war550@live.com.ph>
2016-12-07 14:37:04 +0800

    updated userstableseeder, added user_type_id

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,11 +12,42 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         $user = \App\User::create([
-            'first_name'        => 'insite',
+            'user_type_id'      => 1,
+            'first_name'        => 'admin',
             'last_name'         => 'insite',
             'company'           => 'test company',
             'contact_number'    => '123123123',
-            'email'             => 'insite@test.com',
+            'email'             => 'admin@insite.com',
+            'password'          => bcrypt('password'),
+        ]);
+
+        \App\Models\EventUser::create([
+            'user_id'   => $user->id,
+            'event_id'  => 1
+        ]);
+
+        $user = \App\User::create([
+            'user_type_id'      => 2,
+            'first_name'        => 'client',
+            'last_name'         => 'insite',
+            'company'           => 'test company',
+            'contact_number'    => '123123123',
+            'email'             => 'sampler@insite.com',
+            'password'          => bcrypt('password'),
+        ]);
+
+        \App\Models\EventUser::create([
+            'user_id'   => $user->id,
+            'event_id'  => 1
+        ]);
+
+        $user = \App\User::create([
+            'user_type_id'      => 3,
+            'first_name'        => 'sampler',
+            'last_name'         => 'insite',
+            'company'           => 'test company',
+            'contact_number'    => '123123123',
+            'email'             => 'sampler@insite.com',
             'password'          => bcrypt('password'),
         ]);
 
