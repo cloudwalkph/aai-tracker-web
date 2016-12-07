536cfdcb46d875590d097d657c5c873d1ae10d02
Alleo Indong <freedom_war550@live.com.ph>
2016-12-07 15:03:35 +0800

    added login controller for admin

---
+++ app/Http/Controllers/Management/LoginController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Http\Controllers\Management;
+
+use App\Http\Controllers\Controller;
+
+class LoginController extends Controller {
+    public function index()
+    {
+        config(['app.name' => 'Insite Management Login']);
+
+        return view('auth.login');
+    }
+}
