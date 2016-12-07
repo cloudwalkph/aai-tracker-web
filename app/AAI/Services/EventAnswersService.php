382039705d7891864e320264fef0b28f72ba9ba7
Alleo Indong <freedom_war550@live.com.ph>
2016-12-07 13:07:15 +0800

    updated eventanswerid return

--- app/AAI/Services/EventAnswersService.php
+++ app/AAI/Services/EventAnswersService.php
@@ -89,24 +89,30 @@ class EventAnswersService {
         $eventLocationAnswers = EventLocationAnswer::where('event_id', $eventId)
             ->where('user_id', $userId)->get();
 
-        $eventPolls = EventPoll::where('event_id', $eventId)->get();
+        $result = [];
+        foreach ($eventLocationAnswers as $locationAnswer) {
+            $locationAnswer['answers'] = $this->getPollsData($locationAnswer);
+
+            $result[] = $locationAnswer;
+        }
+
+        return $result;
+    }
+
+    private function getPollsData($locationAnswer)
+    {
+        $eventPolls = EventPoll::where('event_id', $locationAnswer->event_id)->get();
 
         $result = [];
         foreach ($eventPolls as $poll) {
             $pollInfo = Poll::where('id', $poll->poll_id)->first();
 
-            // set key for poll
-            foreach ($eventLocationAnswers as $locationAnswer) {
-                $eventAnswers = EventAnswer::where('event_location_answer_id', $locationAnswer->id)
-                    ->where('poll_id', $pollInfo->id)
-                    ->get();
+            $eventAnswers = EventAnswer::where('event_location_answer_id', $locationAnswer->id)
+                ->where('poll_id', $pollInfo->id)
+                ->get();
 
-                foreach ($eventAnswers as $answer) {
-                    $result[$pollInfo->name][] = [
-                        'answer' => 1,
-                        'label'  => ucwords($answer->value)
-                    ];
-                }
+            foreach ($eventAnswers as $answer) {
+                $result[] = $answer;
             }
         }
 
