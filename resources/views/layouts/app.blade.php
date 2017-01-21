5f2a58504f2d0263430f17d12bcd21b7fa9709cf
Alleo Indong <freedom_war550@live.com.ph>
2017-01-21 22:48:31 +0800

    updated app.blade.php for menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -165,19 +165,19 @@ desired effect
             <ul class="sidebar-menu">
                 <li class="header">HEADER</li>
                 <!-- Optionally, you can add icons to the links -->
-                <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
-                <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
-                <li class="treeview">
-                    <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
-                        <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li><a href="#">Link in level 2</a></li>
-                        <li><a href="#">Link in level 2</a></li>
-                    </ul>
-                </li>
+                <li class="active"><a href="/management/events"><i class="fa fa-link"></i> <span>Events</span></a></li>
+                <li><a href="/management/users"><i class="fa fa-link"></i> <span>Users</span></a></li>
+                {{--<li class="treeview">--}}
+                    {{--<a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>--}}
+                        {{--<span class="pull-right-container">--}}
+                            {{--<i class="fa fa-angle-left pull-right"></i>--}}
+                        {{--</span>--}}
+                    {{--</a>--}}
+                    {{--<ul class="treeview-menu">--}}
+                        {{--<li><a href="#">Link in level 2</a></li>--}}
+                        {{--<li><a href="#">Link in level 2</a></li>--}}
+                    {{--</ul>--}}
+                {{--</li>--}}
             </ul>
             <!-- /.sidebar-menu -->
         </section>
